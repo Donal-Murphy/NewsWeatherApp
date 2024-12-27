014ab6662c01e73f08a4c392c8c9b48533f8b5ce
Donal Murphy <donal.murphy@outlook.ie>
2024-12-27 16:31:22 +0000

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G00438847@atu.ie',
+  webDir: 'www'
+};
+
+export default config;
