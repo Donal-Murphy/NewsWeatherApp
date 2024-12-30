46eed1dc73a0ca32939d9960ae183be0c3956d31
Donal Murphy <donal.murphy@outlook.ie>
2024-12-30 15:09:29 +0000

    Implemented Settings & Storage

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonList, IonCheckbox, IonLabel } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCheckbox, IonList, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel]
 })
 export class SettingsPage implements OnInit {
+  selectedOption: string = 'Metric'; // Default units option
 
-  constructor() { }
+  constructor(private MyDataService: MyDataService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    // Load the stored units option
+    const storedOption = await this.MyDataService.get('units');
+    this.selectedOption = storedOption || 'Metric';
+  }
+
+  async onOptionChange(option: string) {
+    this.selectedOption = option;
+    await this.MyDataService.set('units', option); // Save selected units option
   }
 
 }
